155564cf23df2ec70dd54277703063abd19123a4
Kasper Seweryn <casper@wvffle.net>
2019-06-13 12:28:58 +0200

    Close #9

---
+++ components/panda-news.js
@@ -0,0 +1,14 @@
+const component = {
+  name: `panda-news`,
+  lazy: false,
+  css: [],
+  js: {},
+  props: [],
+  data () {
+    return {
+      name: `panda-news`,
+    }
+  },
+}
+component.waffle_style = true
+component.waffle_template = true
