9f99cfa44bd60f96815a3d797e28ead4ddb56bcc
Kasper Seweryn <github@wvffle.net>
2020-01-09 00:43:28 +0100

    Add part of netlify cms

--- gridsome.config.js
+++ gridsome.config.js
@@ -6,5 +6,35 @@
 
 module.exports = {
   siteName: 'Pandasite',
-  plugins: []
+  transformers: {
+    remark: {
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+      anchorClassName: 'icon icon-link',
+      plugins: [
+        // ...global plugins
+      ]
+    }
+  },
+
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'posts/**/*.md',
+        typeName: 'Post',
+        remark: {
+          plugins: [
+            // ...local plugins
+          ]
+        }
+      }
+    },
+    {
+      use: `gridsome-plugin-netlify-cms`,
+      options: {
+        publicPath: `/admin`
+      }
+    },
+  ]
 }
