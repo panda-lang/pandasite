02a7ee726005c77ce979beea1ae8358b24c0d846
dzikoysk <dzikoysk@dzikoysk.net>
2020-03-18 20:45:07 +0100

    GH-12 Support mobile devices by the Index layout

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,57 +5,57 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Pandasite',
-  transformers: {
-    remark: {
-      externalLinksTarget: '_blank',
-      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
-      anchorClassName: 'icon icon-link',
-      plugins: [
-        // ...global plugins
-      ]
-    }
-  },
-
-  plugins: [
-    {
-      use: '@gridsome/source-filesystem',
-      options: {
-        path: 'posts/**/*.md',
-        typeName: 'Post',
+    siteName: 'Pandasite',
+    transformers: {
         remark: {
-          plugins: [
-            // ...local plugins
-          ]
+            externalLinksTarget: '_blank',
+            externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+            anchorClassName: 'icon icon-link',
+            plugins: [
+                // ...global plugins
+            ]
         }
-      }
     },
-    {
-      use: `gridsome-plugin-netlify-cms`,
-      options: {
-        publicPath: `/admin`
-      }
-    },
-    {
-      use: '@gridsome/plugin-critical',
-      options: {
-        paths: ['/'],
-        width: 1300,
-        height: 900
-      }
-    },
-    {
-      use: 'gridsome-plugin-pug',
-      options: {
-        pug: { /* Options for `pug-plain-loader` */ },
-        pugLoader: { /* Options for `pug-loader` */ }
-      }
-    },
-    {
-      use: "gridsome-plugin-tailwindcss"
-    }
-  ],
-  chainWebpack (config) {
 
-  }
+    plugins: [
+        {
+            use: '@gridsome/source-filesystem',
+            options: {
+                path: 'posts/**/*.md',
+                typeName: 'Post',
+                remark: {
+                    plugins: [
+                        // ...local plugins
+                    ]
+                }
+            }
+        },
+        {
+            use: `gridsome-plugin-netlify-cms`,
+            options: {
+                publicPath: `/admin`
+            }
+        },
+        {
+            use: '@gridsome/plugin-critical',
+            options: {
+                paths: ['/'],
+                width: 1300,
+                height: 900
+            }
+        },
+        {
+            use: 'gridsome-plugin-pug',
+            options: {
+                pug: { /* Options for `pug-plain-loader` */},
+                pugLoader: { /* Options for `pug-loader` */}
+            }
+        },
+        {
+            use: "gridsome-plugin-tailwindcss"
+        }
+    ],
+    chainWebpack(config) {
+
+    }
 }
