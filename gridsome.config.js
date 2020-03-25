7e6ff553ae3050623b4466e51b5e15ae6e938b2e
Kasper Seweryn <github@wvffle.net>
2020-03-25 13:55:49 +0100

    Lint with standard

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,24 +1,24 @@
-// This is where project configuration and plugin options are located. 
+// This is where project configuration and plugin options are located.
 // Learn more: https://gridsome.org/docs/config
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-    siteName: 'Pandasite',
-    plugins: [
-        {
-            use: 'gridsome-plugin-pug',
-            options: {
-                pug: { /* Options for `pug-plain-loader` */ },
-                pugLoader: { /* Options for `pug-loader` */ }
-            }
-        },
-        {
-            use: 'gridsome-plugin-tailwindcss'
-        },
-        {
-            use: 'gridsome-plugin-purgecss'
-        }
-    ]
+  siteName: 'Pandasite',
+  plugins: [
+    {
+      use: 'gridsome-plugin-pug',
+      options: {
+        pug: { /* Options for `pug-plain-loader` */ },
+        pugLoader: { /* Options for `pug-loader` */ }
+      }
+    },
+    {
+      use: 'gridsome-plugin-tailwindcss'
+    },
+    {
+      use: 'gridsome-plugin-purgecss'
+    }
+  ]
 }
