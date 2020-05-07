a017b757ae2e80c6ca51ea8cb2daf9d766ac39fa
dzikoysk <dzikoysk@dzikoysk.net>
2020-05-08 01:48:15 +0200

    Deploy Gridsome based Pandasite

---
+++ assets/js/page--src-pages-page-not-found-vue.7df5f886.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{397:function(t,n,e){"use strict";e.r(n);var a={metaInfo:{title:"PageNotFound"}},o=e(37),s=Object(o.a)(a,(function(){var t=this.$createElement,n=this._self._c||t;return n("Layout",[n("div",{staticClass:"container mx-auto"},[n("h1",[this._v("404")])])])}),[],!1,null,null,null);n.default=s.exports}}]);
